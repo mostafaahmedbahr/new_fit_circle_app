dea102ad042f80d9e35810e2c806c24ee6125907
PC <mustafa.bahr12323@gmail.com>
2024-09-15 09:30:01 +0300

    15/9/2024

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,11 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:new_fit_circle_last_ui/dio/sh/sh.dart';
+import 'package:new_fit_circle_last_ui/screens/player_screens/articles_details/articles_details_screen.dart';
 import 'package:new_fit_circle_last_ui/screens/player_screens/auth/login/login_screen.dart';
+import 'package:new_fit_circle_last_ui/screens/player_screens/auth/register/register_screen.dart';
+import 'package:new_fit_circle_last_ui/screens/player_screens/coash_details/coash_details_screen.dart';
+import 'package:new_fit_circle_last_ui/screens/player_screens/courses_for_player/courses_for_player_screen.dart';
 import 'package:new_fit_circle_last_ui/screens/player_screens/join_to_course/cubit/cubit.dart';
 import 'package:new_fit_circle_last_ui/screens/player_screens/join_to_course/general_question_screen1.dart';
 import 'package:new_fit_circle_last_ui/screens/player_screens/layout_player/layout_player.dart';
@@ -61,7 +65,7 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Roboto',
         ),
         debugShowCheckedModeBanner: false,
-        home: const GeneralQuestionScreen1(),
+        home: const CoashDetailsScreen(),
         builder: (context, child) => ResponsiveWrapper.builder(
           BouncingScrollWrapper.builder(context, child!),
           maxWidth: 1200,
